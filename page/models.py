d1ab8cf2727870b2c91b43837190d21b06acbd76
Tomas Guida <tomasguida2000@hotmail.com>
2023-03-23 15:36:41 -0300

    migrations

--- page/models.py
+++ page/models.py
@@ -2,8 +2,9 @@ from django.db import models
 
 class game(models.Model):
     vgame_name = models.CharField(max_length=30)
+    img = models.CharField(max_length=200)
     developer_name = models.CharField(max_length=30)
     year = models.IntegerField
-    description = models.TextField
-    categories = models.CharField
+    description = models.TextField(max_length=500)
+    categories = models.CharField(max_length=100)
     
